8540862d6bad9a892df3f4123146e1d6dc136e4f
YuHyunO <107340210+YuHyunO@users.noreply.github.com>
2024-09-14 00:34:46 +0900

    Update SessionCleanupCallback.java

--- src/main/java/mb/dnm/core/callback/SessionCleanupCallback.java
+++ src/main/java/mb/dnm/core/callback/SessionCleanupCallback.java
@@ -7,7 +7,7 @@ import mb.dnm.core.context.ServiceContext;
 import java.util.Map;
 
 /**
- * 하나의 서비스 프로세스가 종료될 때 마다 <code>ClosableSession</code> 인터페이스를 구현하는 객체들의 Stream을 모두 닫아주기 위한 Callback 클래스이다.<br>
+ * 하나의 서비스 프로세스가 종료될 때 마다 <code>ClosableStreamWrapper</code> 인터페이스를 구현하는 객체들의 Stream을 모두 닫아주기 위한 Callback 클래스이다.<br>
  * <code>ServiceProcessor</code>에 기본 callback으로 등록되어 있다.
  *
  * @see mb.dnm.core.callback.AfterProcessCallback
