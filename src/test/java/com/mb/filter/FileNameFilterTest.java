2a893b74a6763ebb5c499bab6a56d69302f9cba8
YuHyunO <wwdg0913@gmail.com>
2024-10-10 18:00:10 +0900

    Update FileNameFilterTest.java

--- src/test/java/com/mb/filter/FileNameFilterTest.java
+++ src/test/java/com/mb/filter/FileNameFilterTest.java
@@ -1,6 +1,7 @@
 package com.mb.filter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.junit.Test;
 
 @Slf4j
@@ -13,7 +14,10 @@ public class FileNameFilterTest {
 
     @Test
     public void fileNameFilter_accept_test() {
+        WildcardFileFilter filter = new WildcardFileFilter("RG0*");
+        String fileName = "RG031011001";
 
+        log.info("{}", filter.accept(null, fileName));
     }
 
 
