4659743370067ac2210fca8e8a1494d682f36255
YuHyunO <wwdg0913@gmail.com>
2024-10-24 14:58:55 +0900

    Update ErrorTrace.java

--- src/main/java/mb/dnm/exeption/ErrorTrace.java
+++ src/main/java/mb/dnm/exeption/ErrorTrace.java
@@ -1,12 +1,15 @@
 package mb.dnm.exeption;
 
+import lombok.Getter;
 import mb.dnm.util.MessageUtil;
 
 import java.io.Serializable;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+
 public class ErrorTrace implements Serializable {
+    private static final long serialVersionUID = -5851150242323943159L;
     private String message;
     private String localizedMessage;
     private StringBuffer traceBuffer;
@@ -26,6 +29,13 @@ public class ErrorTrace implements Serializable {
         return traceMap;
     }
 
-
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ErrorTrace{");
+        sb.append("message='").append(message).append('\'');
+        sb.append(", localizedMessage='").append(localizedMessage).append('\'');
+        sb.append(", traceBuffer=").append(traceBuffer);
+        sb.append('}');
+        return sb.toString();
+    }
 }
